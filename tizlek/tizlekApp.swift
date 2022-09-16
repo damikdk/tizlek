8705b8dcf86fd1e6aa01be0beed1d0d1bdd2ec0d
Damik Minnegalimov <damikdk@gmail.com>
2022-09-17 01:51:13 +0300

    Initial Commit

---
+++ tizlek/tizlekApp.swift
@@ -0,0 +1,17 @@
+//
+//  tizlekApp.swift
+//  tizlek
+//
+//  Created by Damir Minnegalimov on 17.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct tizlekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
